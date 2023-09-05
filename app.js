bbd0f7c2f7673e024267387f6102a8c7da7f8050
秋葉桃子 <sora.super@icloud.com>
2023-09-05 22:25:22 +0900

    add function todo-add done

---
+++ app.js
@@ -0,0 +1,6 @@
+$("#addTodo").click(function ()　{
+    const inputTodo = $("input").val();
+    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+
+    $("input").val("");
+});
